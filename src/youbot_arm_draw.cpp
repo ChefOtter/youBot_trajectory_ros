65d46043c02a854f3f501a10fc5378e34ad0fba6
Chao Yao <chao.yao@tu-dresden.de>
2016-02-19 10:39:31 +0100

    still in develop.

--- src/youbot_arm_draw.cpp
+++ src/youbot_arm_draw.cpp
@@ -26,7 +26,7 @@ int main (int argc, char** argv)
 //	sleep(20);
 
 	// setup MoveGroup
-	moveit::planning_interface::MoveGroup group("arm_1");
+	moveit::planning_interface::MoveGroup group("my_arm");
 	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
 	moveit_msgs::DisplayTrajectory display_trajectory;
@@ -34,16 +34,16 @@ int main (int argc, char** argv)
 	ros::Publisher display_publisher = nh.advertise<moveit_msgs::DisplayTrajectory>("/move_group/display_planned_path",1,true);
 
 	std::string REF_FRAME = "arm_link_0";
-	std::string EEF_LINK = "arm_link_5";
+//	std::string EEF_LINK = "arm_link_5";
 
 	// configure the group
 	group.setPoseReferenceFrame(REF_FRAME);
-	group.setEndEffectorLink(EEF_LINK);
+//	group.setEndEffectorLink(EEF_LINK);
 
 	group.setPlannerId("RRTConnectkConfigDefault");
 	// set tolerance
 	group.setGoalJointTolerance(0.001);
-	group.setGoalPositionTolerance(0.001);
+	group.setGoalPositionTolerance(0.01);
 	group.setGoalOrientationTolerance(1);
 
 	// show the name of the reference frame
@@ -57,61 +57,60 @@ int main (int argc, char** argv)
 	ROS_INFO("Arm has been moved to home position.");
 	sleep(2.0);
 
-	// move straight forward
-	geometry_msgs::Pose target_pose0;
-	target_pose0.position.x = 0.2;
-	target_pose0.position.y = 0.0;
-	target_pose0.position.z = 0.4;
+	// move to home position
+	group.setNamedTarget("ready");
+	group.move();
+	ROS_INFO("Arm has been moved to ready position.");
+	sleep(2.0);
 
-	target_pose0.orientation.x = 0.0;
-	target_pose0.orientation.y = sqrt(2.0)/2.0;
-	target_pose0.orientation.z = 0.0;
-	target_pose0.orientation.w = sqrt(2.0)/2.0;
+	// move forward
+	geometry_msgs::PoseStamped current_pose = group.getCurrentPose();
+	geometry_msgs::Pose target_pose0 = current_pose.pose;
+	target_pose0.position.x += 0.1;
 
-	group.setPoseTarget(target_pose0,"arm_link_5");
+	group.setPoseTarget(target_pose0);
 //	group.setPositionTarget(0.214291, 0.0, 0.417,"arm_link_5");
 	moveit::planning_interface::MoveGroup::Plan plan;
 	bool success = group.plan(plan);
 	ROS_INFO("Test 1 (work space goal): %s", success ? "Success" : "Failed");
 	group.move();
 
-	sleep(2.0);
+//	sleep(2.0);
+//
+//	std::vector<double> arm_joint_values;
+//	group.getCurrentState()->copyJointGroupPositions(group.getCurrentState()->getRobotModel()->getJointModelGroup(group.getName()),arm_joint_values);
+//	arm_joint_values[4] = 2.9234;
+//	group.setJointValueTarget(arm_joint_values);
+//	success = group.plan(plan);
+//	ROS_INFO("Test 2 (joint space goal): %s", success ? "Success" : "Failed");
+//	group.move();
 
-	std::vector<double> arm_joint_values;
-	group.getCurrentState()->copyJointGroupPositions(group.getCurrentState()->getRobotModel()->getJointModelGroup(group.getName()),arm_joint_values);
-//	group.getJointValueTarget();
-	arm_joint_values[4] = 2.9234;
-	group.setJointValueTarget(arm_joint_values);
-	success = group.plan(plan);
-	ROS_INFO("Test 2 (joint space goal): %s", success ? "Success" : "Failed");
-	group.move();
-
-	sleep(2.0);
-
-	group.setStartStateToCurrentState();
-	group.setPositionTarget(0.2, -0.1, 0.4, "arm_link_5");
-	success = group.plan(plan);
-	group.move();
-
-	group.setStartStateToCurrentState();
-	group.setPositionTarget(0.2, 0.1, 0.4, "arm_link_5");
-	success = group.plan(plan);
-	group.move();
-
-	group.setStartStateToCurrentState();
-	group.setPositionTarget(0.2, 0.1, 0.3, "arm_link_5");
-	success = group.plan(plan);
-	group.move();
-
-	group.setStartStateToCurrentState();
-	group.setPositionTarget(0.2, -0.1, 0.3, "arm_link_5");
-	success = group.plan(plan);
-	group.move();
-
-	group.setStartStateToCurrentState();
-	group.setPositionTarget(0.2, -0.1, 0.4, "arm_link_5");
-	success = group.plan(plan);
-	group.move();
+//	sleep(2.0);
+//
+//	group.setStartStateToCurrentState();
+//	group.setPositionTarget(0.2, -0.1, 0.4, "arm_link_5");
+//	success = group.plan(plan);
+//	group.move();
+//
+//	group.setStartStateToCurrentState();
+//	group.setPositionTarget(0.2, 0.1, 0.4, "arm_link_5");
+//	success = group.plan(plan);
+//	group.move();
+//
+//	group.setStartStateToCurrentState();
+//	group.setPositionTarget(0.2, 0.1, 0.3, "arm_link_5");
+//	success = group.plan(plan);
+//	group.move();
+//
+//	group.setStartStateToCurrentState();
+//	group.setPositionTarget(0.2, -0.1, 0.3, "arm_link_5");
+//	success = group.plan(plan);
+//	group.move();
+//
+//	group.setStartStateToCurrentState();
+//	group.setPositionTarget(0.2, -0.1, 0.4, "arm_link_5");
+//	success = group.plan(plan);
+//	group.move();
 
 	//sleep(5.0);
 	ros::waitForShutdown();
